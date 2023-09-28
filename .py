f395151f7c2c68cdbeadc6cd12551fe4770bcd6d
OrdinaryFabulous <ey320912617@gmail.com>
2023-09-28 13:32:21 -0400

    Update .py

--- .py
+++ .py
@@ -4,4 +4,13 @@ def Open(east, west):
     else:
         return(True)
     
-print(Open(True, True))
+print(Open(False, False))
+
+def whoshockal(x, y):
+    if x == y:
+        return False
+    else:
+        return True
+
+
+print(whoshockal(True, False))
